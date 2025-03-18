e1b003ba2f94566a768df30ed46c35f3e4d56800
Chea-yunzi <katete051@naver.com>
2025-03-18 20:37:04 +0900

    initial commit

--- potato/one_week/app/src/main/java/com/cookandroid/one_week/MainActivity.kt
+++ potato/one_week/app/src/main/java/com/cookandroid/one_week/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.cookandroid.one_week
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +18,35 @@ import com.cookandroid.one_week.ui.theme.One_weekTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_2)
+
+        // 이미지 클릭 시 EmptyActivity로 이동
+        findViewById<ImageView>(R.id.image1).setOnClickListener {
+            val intent = Intent(this, EmptyActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageView>(R.id.image2).setOnClickListener {
+            val intent = Intent(this, EmptyActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageView>(R.id.image3).setOnClickListener {
+            val intent = Intent(this, EmptyActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageView>(R.id.image4).setOnClickListener {
+            val intent = Intent(this, EmptyActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageView>(R.id.image5).setOnClickListener {
+            val intent = Intent(this, EmptyActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
