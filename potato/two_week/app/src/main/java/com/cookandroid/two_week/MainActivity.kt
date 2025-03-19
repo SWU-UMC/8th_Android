4ac499146a767913734c35a2ad1a0e9f60a9081c
Chea-yunzi <katete051@naver.com>
2025-03-19 21:58:18 +0900

    Commit Message

--- potato/two_week/app/src/main/java/com/cookandroid/two_week/MainActivity.kt
+++ potato/two_week/app/src/main/java/com/cookandroid/two_week/MainActivity.kt
@@ -9,7 +9,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.acitivity_main)
+        supportActionBar?.hide() // 액티비티 제목 숨기기
+
+        // 예시로 Fragment1에서 Fragment2로 이동할 때 슬라이드 효과 추가
+        /*val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        // 슬라이드 애니메이션
+        fragmentTransaction.setCustomAnimations(
+            android.R.anim.slide_in_left,   // 화면에 등장할 때 애니메이션
+            android.R.anim.slide_out_right  // 화면에서 사라질 때 애니메이션
+        )*/
+
+
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
@@ -33,7 +45,10 @@ class MainActivity : AppCompatActivity() {
     // Fragment 교체 함수
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out) // 애니메이션 추가
+            .replace(R.id.fragment_container, fragment) // 새로운 Fragment로 교체
+            .commit() // 변경 사항 적용
+        //페이드 아웃(fade out) 효과
+
     }
 }
