2bab42bd3b2375aa5ab946bae2d734fe03264668
kna.sy <soyoung1313m@gmail.com>
2025-04-01 17:42:19 +0900

    refactor: 2주차 미션 findViewById 대신 binding으로 뷰 접근

--- kna/2w/app/src/main/java/com/example/mission_2/MainActivity.kt
+++ kna/2w/app/src/main/java/com/example/mission_2/MainActivity.kt
@@ -6,17 +6,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mission_2.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val fragmentContainer = findViewById<FrameLayout>(R.id.fragmentContainer)
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigationView
+        val fragmentContainer: FrameLayout = binding.fragmentContainer
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, HomeFragment())
