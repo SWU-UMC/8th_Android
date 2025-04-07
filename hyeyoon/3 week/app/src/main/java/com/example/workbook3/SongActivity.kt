3938ed98e5f8668108cf8f22978affdddf16ca3e
simon <agnes20827@gmail.com>
2025-04-07 14:21:47 +0900

    SongActivity 노래 재생 이미지 교체

--- hyeyoon/3 week/app/src/main/java/com/example/workbook3/SongActivity.kt
+++ hyeyoon/3 week/app/src/main/java/com/example/workbook3/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.workbook3
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,22 @@ class SongActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.songDownIb.setOnClickListener {
             finish()
+        }
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+    }
+    fun setPlayerStatus(isPlaying : Boolean) {
+        if(isPlaying){
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        }
+        else {
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
         }
     }
 }
