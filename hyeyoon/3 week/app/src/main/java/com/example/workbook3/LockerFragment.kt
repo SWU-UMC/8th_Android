beee3a1aa929eb90ebea4d902dc59247b1573de1
simon <agnes20827@gmail.com>
2025-04-08 04:24:51 +0900

    LockerFragment 화면 TabLayout과 ViewPager로 구현

--- hyeyoon/3 week/app/src/main/java/com/example/workbook3/LockerFragment.kt
+++ hyeyoon/3 week/app/src/main/java/com/example/workbook3/LockerFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.workbook3.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class LockerFragment : Fragment() {
@@ -16,6 +17,17 @@ class LockerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        var lockerVPAdapter = LockerVPAdapter(this)
+        binding.lockerViewpager.adapter = lockerVPAdapter
+
+        TabLayoutMediator(binding.lockerTabLayout, binding.lockerViewpager) {
+                tab, position ->
+            tab.text = when(position) {
+                0 -> "저장한 곡"
+                else -> "음악파일"
+            }
+        }.attach()
         return binding.root
     }
 }
