38121ee50c32d9734bbf02b77155c82398580178
simon <agnes20827@gmail.com>
2025-04-07 13:59:11 +0900

    SongActivity 종료

---
+++ hyeyoon/3 week/app/src/main/java/com/example/workbook3/SongActivity.kt
@@ -0,0 +1,21 @@
+package com.example.workbook3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.workbook3.databinding.ActivitySongBinding
+
+class SongActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+    }
+}
