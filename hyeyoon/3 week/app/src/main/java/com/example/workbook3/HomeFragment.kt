627cc72eed36fd22e7ff8139d6b64bf54bc4d513
simon <agnes20827@gmail.com>
2025-04-08 18:43:13 +0900

    패널 viewpager

--- hyeyoon/3 week/app/src/main/java/com/example/workbook3/HomeFragment.kt
+++ hyeyoon/3 week/app/src/main/java/com/example/workbook3/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.workbook3
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +12,39 @@ import com.example.workbook3.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private var currentPage = 0
+    private val slideHandler = Handler(Looper.getMainLooper())
+    private lateinit var slideRunnable: Runnable
     lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val adapter = HomeBackgroundVpAdapter(this)
+        binding.homePannelBackgroundVp.adapter = adapter
+        slideRunnable = object : Runnable {
+            override fun run() {
+                val pageCount = adapter.itemCount
+                currentPage = (currentPage + 1) % pageCount
+                binding.homePannelBackgroundVp.setCurrentItem(currentPage, true)
+                slideHandler.postDelayed(this, 2000) // 2초마다 실행
+            }
+        }
+        slideHandler.postDelayed(slideRunnable, 2000)
+
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val backgroundImages = listOf(
+            R.drawable.img_first_album_default,
+            R.drawable.img_home_viewpager_exp,
+            R.drawable.img_home_viewpager_exp2
+        )
+
+        val backgroundAdapter = HomeBackgroundVpAdapter(backgroundImages)
+        binding.homePannelBackgroundVp.adapter = backgroundAdapter
+        binding.homeBackgroundIndicator.setViewPager(binding.homePannelBackgroundVp)
+        binding.homePannelBackgroundVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         binding.homeAlbumImgIv1.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
@@ -29,5 +57,15 @@ class HomeFragment : Fragment() {
         binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         return binding.root
+    }
+
+    override fun onPause() {
+        super.onPause()
+        slideHandler.removeCallbacks(slideRunnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        slideHandler.postDelayed(slideRunnable, 2000)
     }
 }
