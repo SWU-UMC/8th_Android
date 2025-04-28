4a8533ae97891b558461d91f0497428975540826
Chea-yunzi <katete051@naver.com>
2025-04-28 16:29:38 +0900

    feat: 내부 저장소 저장 및 시크바 정보 전송 기능 추가

--- potato/flo/app/src/main/java/com/cookandroid/flo/SongActivity.kt
+++ potato/flo/app/src/main/java/com/cookandroid/flo/SongActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.cookandroid.flo.databinding.ActivitySongBinding
+import com.google.gson.Gson
 
 
 class SongActivity : AppCompatActivity() {
@@ -22,6 +23,9 @@ class SongActivity : AppCompatActivity() {
 
     //미디어 플레이어를 이용. 음악을 재생
     private var mediaPlayer : MediaPlayer? = null //음악 값이 널일 수 있기 때문.
+
+    //Gson 소환!
+    private var gson : Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,6 +79,20 @@ class SongActivity : AppCompatActivity() {
     override fun onPause(){
         super.onPause()
         setPlayerStatus(false)
+        song.second = ((binding.songProgressbarSb.progress * song.playtime)/100)/1000 //미니 플레이어에 기존에 재생되고 있던 쏭데이터를 반영!
+        //1000으로 나누는 이유 -> 초 단위로 계산중!
+
+        //데이터를 저장!
+        //내부 저장시 앱에 저장할 수 있는것이 SharedPreference.(로그인 기억하기!)
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val editor = sharedPreferences.edit() //에디터가 필요함.
+        //editor.putString("title", song.title) //song 정보를.. 다.. 하기 번거로워! json으로 데이터를 전송할 것이다!
+        //자바 객체를 -> json => Gson! -> SharedPreference에 넣을 것이다
+        val songJson = gson.toJson(song) //쥐슨이 json 포멧으로 변환
+        editor.putString("song", songJson)
+
+        editor.apply() //실제 저장작업 완료! 꼭 있어야 해(커밋, 푸쉬랑 비슷한 개념)
+        
 
     }
 
