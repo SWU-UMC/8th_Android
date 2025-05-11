9569b7afd6a538e70fa3f34db929dfc39d843f32
Chea-yunzi <katete051@naver.com>
2025-05-11 18:55:07 +0900

    feat: 7주차 실습 전, 데이터 베이스 설정

--- potato/flo/app/src/main/java/com/cookandroid/flo/Song.kt
+++ potato/flo/app/src/main/java/com/cookandroid/flo/Song.kt
@@ -1,7 +1,11 @@
 package com.cookandroid.flo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //제목, 가수, 사진,재생시간,현재 재생시간, isplaying(재생 되고 있는지)
 
+@Entity(tableName = "SongTable")
 data class Song(
     var title : String = "",
     var singer : String = "",
@@ -9,6 +13,12 @@ data class Song(
     var playtime : Int = 0,
     var isPlaying : Boolean = false,
     //어떤 음악이 재생되는지를 알려줌
-    var music: String = ""
+    var music: String = "",
 
-)
+    //7주차 데이터 베이스 사용을 위한 코드 추가
+    var coverImg: Int? = null,
+    var isLike: Boolean = false
+
+){
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
---
+++ potato/flo/app/src/main/java/com/cookandroid/flo/SongDatabase.kt
@@ -0,0 +1,32 @@
+package com.cookandroid.flo
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Song::class], version =1 )
+abstract class SongDatabase: RoomDatabase() {
+
+    companion object{
+        private var instance : SongDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context) : SongDatabase?{
+            if(instance == null)
+            {
+                synchronized(SongDatabase::class){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SongDatabase::class.java,
+                        "song-database" //다른 데이터 베이스랑 이름 겹치면 꼬임
+
+                    ).allowMainThreadQueries().build() //쓰레드랑 데이터베이스 연동
+                }
+            }
+
+            return instance
+        }
+    }
+}
+
